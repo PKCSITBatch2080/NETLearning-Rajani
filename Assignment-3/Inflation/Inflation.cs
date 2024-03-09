30ebff75dfa533c9eb3a4542c06f97ef525d8ef8
Rajani1-tech <rajanilami.12@gmail.com>
2024-03-09 17:46:15 +0545

    Done Assignment-3

---
+++ Assignment-3/Inflation/Inflation.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Inflation
+{
+    public string RegionalMember { get; set; }
+    public int Year { get; set; }
+    public double Inflation { get; set; }
+    public string UnitOfMeasurement { get; set; }
+    public string Subregion { get; set; }
+    public string CountryCode { get; set; }
+}
