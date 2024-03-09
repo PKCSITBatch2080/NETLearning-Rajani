8c6a5d774737494e40350845562c21d648dee172
Rajani1-tech <rajanilami.12@gmail.com>
2024-03-09 17:27:30 +0545

    read the csv data

---
+++ Assignment-3/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var analysis = new InflationAnalysis();
+        analysis.ReadCSV(@"Assignment 3\Inflation.csv");
+        
+
+    }
+}
