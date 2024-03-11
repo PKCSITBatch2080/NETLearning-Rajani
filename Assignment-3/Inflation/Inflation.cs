e25f155391bc01394c64dca3279d555a1924dae8
Rajani1-tech <rajanilami.12@gmail.com>
2024-03-11 16:50:59 +0545

    solved error

--- Assignment-3/Inflation/Inflation.cs
+++ Assignment-3/Inflation/Inflation.cs
@@ -1,12 +1,13 @@
-namespace InflationAnalyzer
+public class Inflation
 {
-    public class Inflation
-    {
-        public string RegionalMember { get; set; }
-        public int Year { get; set; }
-        public double InflationRate { get; set; }
-        public string UnitOfMeasurement { get; set; }
-        public string Subregion { get; set; }
-        public string CountryCode { get; set; }
-    }
+    public string RegionalMember{get; set;}
+    public int Year{get; set;}
+
+    public double InflationRate{get; set;}
+
+    public string UnitOfMeasurement{get; set;}
+
+    public string Subregion{get; set;}
+
+    public string CountryCode{get; set;}
 }
--- Assignment-3/Inflation/Program.cs
+++ Assignment-3/Inflation/Program.cs
@@ -1,44 +1,34 @@
-﻿using System;
-
-namespace Inflation
+﻿﻿﻿class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string filePath = @"/home/rajani/Desktop/NewFolder/NETLearning-Rajani/Assignment-3/Inflation/Inflation.csv";
+        InflationAnalysis analysis = new InflationAnalysis();
+        analysis.ReadInflationData(filePath);
+
+        Console.WriteLine("Inflation rates for countries for the year 2021:");
+        var data = analysis.GetInflationRatesForYear(2021);
+        foreach (var inflation in data)
         {
-            var analysis = new InflationAnalysis();
-            analysis.ReadCSV("Inflation.csv");
-
-            // Query 1: Inflation rates for countries for the year 2021
-            var inflation2021 = analysis.InflationRatesForYear(2021);
-            Console.WriteLine("Inflation rates for countries for the year 2021:");
-            foreach (var inflation in inflation2021)
-            {
-                Console.WriteLine($"{inflation.RegionalMember}: {inflation.Inflation}");
-            }
-            Console.WriteLine();
-
-            // Query 2: A year when Nepal has the highest inflation
-            var highestInflationYear = analysis.HighestInflationYearForNepal();
-            Console.WriteLine($"Year with the highest inflation in Nepal: {highestInflationYear}");
-            Console.WriteLine();
-
-            // Query 3: List top 10 regions (countries) where inflation is highest for all time
-            var topCountries = analysis.TopCountriesHighestInflation();
-            Console.WriteLine("Top 10 countries with highest inflation:");
-            foreach (var country in topCountries)
-            {
-                Console.WriteLine($"{country.Key}: {country.Value}");
-            }
-            Console.WriteLine();
-
-            // Query 4: List top 3 south Asian countries with the lowest inflation rate for the year 2020
-            var lowestInflation2020 = analysis.TopSouthAsianCountriesLowestInflation(2020);
-            Console.WriteLine("Top 3 South Asian countries with lowest inflation in 2020:");
-            foreach (var country in lowestInflation2020)
-            {
-                Console.WriteLine($"{country.Key}: {country.Value}");
-            }
+            Console.WriteLine($"{inflation.RegionalMember}: {inflation.InflationRate}%");
         }
+
+        Console.WriteLine("\nYear when Nepal has highest inflation:");
+        string countryName = "Nepal";
+        int yearWithHighestInflationForNepal = analysis.GetYearWithHighestInflationForCountry(countryName);
+        Console.WriteLine(yearWithHighestInflationForNepal);
+
+        Console.WriteLine("\nTop 10 regions (countries) where inflation is highest for all time:");
+        foreach (var inflation in analysis.GetTop10RegionsWithHighestInflation())
+        {
+            Console.WriteLine($"{inflation.RegionalMember}: {inflation.InflationRate}%");
+        }
+
+        Console.WriteLine("\nTop 3 South Asian countries with lowest inflation rate for year 2020:");
+        foreach (var inflation in analysis.GetTop3SouthAsianCountriesWithLowestInflationForYear(2020))
+        {
+            Console.WriteLine($"{inflation.RegionalMember}: {inflation.InflationRate}%");
+        }
+
     }
 }
